70a5a4e2a2635355502a7cd817777e0f9355909a
Olivier Duclos <oduclos@sierrawireless.com>
2018-06-18 14:07:32 +0200

    Add first test

---
+++ airvantage_test.go
@@ -0,0 +1,35 @@
+package airvantage
+
+import (
+	"os"
+	"testing"
+)
+
+// The tests need to be sequential.
+func TestMain(t *testing.T) {
+	av, err := NewClient("https://qa.airvantage.io",
+		os.Getenv("API_KEY"), os.Getenv("API_SECRET"),
+		os.Getenv("AV_LOGIN"), os.Getenv("AV_PASSWORD"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	av.Debug = true
+
+	// Create a new System
+	sysspec := System{
+		Name: "api test",
+		Gateway: Gateway{
+			IMEI: "118218318418",
+			Type: "api-gateway",
+		},
+	}
+	sys, err := av.CreateSystem(&sysspec, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if sys.Name != sysspec.Name {
+		t.Fail()
+	}
+}
